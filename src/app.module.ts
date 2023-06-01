f360cfa92493995fe9bc344e0dadd97504539072
myyrakle <sssang97@naver.com>
2023-06-02 00:21:39 +0900

    add: auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [WalletModule, DatabaseModule, UserModule],
+  imports: [WalletModule, DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
